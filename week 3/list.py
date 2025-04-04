c7da00887b925bf101c338e0402f91c61a6d9911
Vineeth6302 <vineethgoudd05@gmail.com>
2025-04-04 15:28:32 +0530

    Create list.py

---
+++ week 3/list.py
@@ -0,0 +1,7 @@
+elements=[]
+limit=int(input("Enter the length of the list"))
+for i in range(limit):
+    temp=int(input("Enter:"))
+    elements.append(temp)
+for i in range(len(elements)):
+    print(elements)
